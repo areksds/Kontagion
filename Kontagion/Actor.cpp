fd06a657476ae21964f46c250d9a4a7fc6474cbe
Arek Der-Sarkissian <areksds@gmail.com>
2020-02-15 12:22:48 -0800

    Making some progress

--- Kontagion/Actor.cpp
+++ Kontagion/Actor.cpp
@@ -1,5 +1,41 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
+/*
+ ACTOR FUNCTIONS
+ */
+
+Actor::Actor(int health, int image, double x, double y, Direction dir, int depth) : GraphObject(image, x, y, dir, depth), m_health(health), m_alive(true) {}
+
+bool Actor::isAlive()
+{
+    return m_alive;
+}
+
+int Actor::getHealth()
+{
+    return m_health;
+}
+
+void Actor::removeHealth(int h)
+{
+    if (m_health != -1)
+    {
+        m_health -= h;
+        if (m_health <= 0)
+            kill();
+    }
+}
+
+void Actor::kill()
+{
+    m_alive = false;
+}
+
+/*
+ DIRT FUNCTIONS
+ */
+
+Dirt::Dirt(double x, double y) : Actor(-1, IID_DIRT, x, y, 90, 1) {}
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
--- Kontagion/Actor.h
+++ Kontagion/Actor.h
@@ -5,4 +5,37 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+/*
+ ACTOR BASE CLASS
+ */
+
+class Actor : public GraphObject
+{
+   public:
+    Actor(int health, int image, double x, double y, Direction dir, int depth);
+    virtual void doSomething() = 0;
+    bool isAlive();
+    int getHealth();
+    void removeHealth(int h);
+    void kill();
+   private:
+    bool m_alive;
+    int m_health;
+};
+
+/*
+ SOCRATES
+ */
+
+/*
+ DIRT
+ */
+
+class Dirt : public Actor
+{
+   public:
+    Dirt(double x, double y);
+    void doSomething() {}
+};
+
 #endif // ACTOR_H_
